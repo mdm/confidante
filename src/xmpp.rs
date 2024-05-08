c96a99259fdfed916360240bd05dbba724a3f252
Marc Dominik Migge <marcmigge@gmx.net>
2024-05-08 19:20:43 +0200

    Add XmppStream and StreamInfo structs + helpers

--- src/xmpp.rs
+++ src/xmpp.rs
@@ -1,13 +1,16 @@
 use std::collections::HashSet;
 
+use anyhow::Error;
 use base64::prelude::*;
 use rand::{RngCore, SeedableRng};
-use tokio::io::{ReadHalf, WriteHalf};
+use tokio::io::{split, ReadHalf, WriteHalf};
 
 use crate::{
-    inbound::connection::Connection,
+    inbound::connection::{self, Connection},
+    settings::get_settings,
     xml::{
-        stream_parser::rusty_xml::StreamParser as ConcreteStreamParser, stream_writer::StreamWriter,
+        stream_parser::{rusty_xml::StreamParser as ConcreteStreamParser, StreamParser},
+        stream_writer::StreamWriter,
     },
 };
 
@@ -50,12 +53,66 @@ struct XmppStream<C>
 where
     C: Connection,
 {
+    starttls_allowed: bool,
+    reader: ConcreteStreamParser<ReadHalf<C>>,
+    writer: StreamWriter<WriteHalf<C>>,
+}
+
+impl<C> XmppStream<C>
+where
+    C: Connection,
+{
+    pub fn new(connection: C) -> Self {
+        let starttls_allowed = connection.is_starttls_allowed();
+        let (reader, writer) = split(connection);
+        let reader = ConcreteStreamParser::new(reader);
+        let writer = StreamWriter::new(writer);
+
+        Self {
+            starttls_allowed,
+            reader,
+            writer,
+        }
+    }
+
+    pub fn reset(mut self) -> XmppStream<C> {
+        let reader = self.reader.into_inner();
+        let writer = self.writer.into_inner();
+        self.reader = ConcreteStreamParser::new(reader);
+        self.writer = StreamWriter::new(writer);
+
+        self
+    }
+
+    pub fn is_starttls_allowed(&self) -> bool {
+        self.starttls_allowed
+    }
+
+    pub async fn upgrade_to_tls(mut self) -> Result<XmppStream<C>, Error> {
+        let reader = self.reader.into_inner();
+        let writer = self.writer.into_inner();
+        let connection = reader.unsplit(writer);
+
+        let connection = connection
+            .upgrade(get_settings().tls.server_config.clone())?
+            .await?;
+
+        let (reader, writer) = split(connection);
+        self.reader = ConcreteStreamParser::new(reader);
+        self.writer = StreamWriter::new(writer);
+
+        Ok(self)
+    }
+}
+
+struct StreamInfo<C>
+where
+    C: Connection,
+{
     stream_id: StreamId,
     jid: Jid,
     peer_jid: Option<Jid>,
     peer_language: LanguageTag,
     connection_type: ConnectionType,
-    negotiated_features: HashSet<StreamFeatures>,
-    parser: ConcreteStreamParser<ReadHalf<C>>,
-    writer: StreamWriter<WriteHalf<C>>,
+    stream: XmppStream<C>,
 }
